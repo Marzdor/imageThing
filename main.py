2b6d1d686cad54711a98179cf430397f375d8289
Marzdor <marzdor69@gmail.com>
2017-05-06 14:38:18 -0700

    More efficent looping
    Added an if statement cutting ~2million calculations on test image and
    saving ~1sec

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from colorMath import *
 from fileOutput import *
 import csv
 
-myImg = Image.open('testImg.jpg')
+myImg = Image.open('1491676804085.png')
 
 imgWidth, imgHeight = myImg.size
 
@@ -19,23 +19,28 @@ with open('ColorList.csv') as infile:
     reader = csv.DictReader(infile, delimiter=';')
     for row in reader:
         listColorRGB = (int(row['R']), int(row['G']), int(row['B']))
-        listOfColors[row['Name']] = (listColorRGB)
+        listOfColors[(listColorRGB)] = row['Name']
 
 for color in pxColor:
 
-    colorLAB1 = convRGBtoLAB(color)
-    bestMatch = [500, " "]
+    if color in listOfColors:
+        pxColorNamed[listOfColors[color]] += pxColor[color]
+    else:
 
-    for listColor in listOfColors:
-        colorLAB2 = convRGBtoLAB(listOfColors[listColor])
+        colorLAB1 = convRGBtoLAB(color)
+        bestMatch = [500, " "]
 
-        dif = deltaE_1994(colorLAB1, colorLAB2)
+        for listColor in listOfColors:
 
-        if dif < bestMatch[0]:
-            bestMatch[0] = dif
-            bestMatch[1] = listColor
+            colorLAB2 = convRGBtoLAB(listColor)
 
-    pxColorNamed[bestMatch[1]] += pxColor[color]
+            dif = deltaE_1994(colorLAB1, colorLAB2)
+
+            if dif < bestMatch[0]:
+                bestMatch[0] = dif
+                bestMatch[1] = listColor
+
+        pxColorNamed[bestMatch[1]] += pxColor[color]
 
 outputDictToFile('pxColor.csv', pxColor)
 outputDictToFile('pxColorNamed.csv', pxColorNamed)
---
+++ pixelCompare.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from colorMath import *
+from fileOutput import *
+from PIL import Image
+import csv
+
+myImg = Image.open('testImg.jpg')
+
+imgWidth, imgHeight = myImg.size
+
+pxColor = defaultdict(int)
+pxColorNamed = defaultdict(int)
+listOfColors = defaultdict(int)
+
+for pixel in list(myImg.getdata()):
+    pxColor[pixel] += 1
+
+with open('ColorList.csv') as infile:
+    reader = csv.DictReader(infile, delimiter=';')
+    for row in reader:
+        listColorRGB = (int(row['R']), int(row['G']), int(row['B']))
+        listOfColors[row['Name']] = (listColorRGB)
+
+for color in pxColor:
+
+    colorLAB1 = convRGBtoLAB(color)
+    bestMatch = [500, " "]
+
+    for listColor in listOfColors:
+        colorLAB2 = convRGBtoLAB(listOfColors[listColor])
+
+        dif = deltaE_1994(colorLAB1, colorLAB2)
+
+        if dif < bestMatch[0]:
+            bestMatch[0] = dif
+            bestMatch[1] = listColor
+
+    pxColorNamed[bestMatch[1]] += pxColor[color]
+
+outputDictToFile('pxColor.csv', pxColor)
+outputDictToFile('pxColorNamed.csv', pxColorNamed)
+outputDictToFile('listOfColors.csv', listOfColors)
